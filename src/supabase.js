7d8f3193d4f82b07351c5a433624346fe437e450
YounesEsso <44702693+YounesEsso@users.noreply.github.com>
2026-05-18 08:38:59 -0500

    Delete src/supabase.js

--- src/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const SUPABASE_URL = 'https://qteeenrednnedkpnqnoi.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF0ZWVlbnJlZG5uZWRrcG5xbm9pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4Njk4MDYsImV4cCI6MjA5NDQ0NTgwNn0.Sib6MYgYt7IONgtzPLWPuty0rRD1lxRxlqrCYNx6QWQ'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
